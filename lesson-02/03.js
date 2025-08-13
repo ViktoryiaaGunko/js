418c7b52d7fe2de58ce43084900f2c87a10fd1a7
Gunko Vika <vikahunko@icloud.com>
2025-08-14 02:56:31 +0300

    second task

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,20 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
+const score = Math.floor(Math.random()*99) +1// тестовое значение, можно изменять
+let grade=0
 // your code
+if (score >= 0 && score < 50) {
+    grade = 'F';
+} else if (score >= 50 && score < 70) {
+    grade = 'D';
+} else if (score >= 70 && score < 80) {
+    grade = 'C';
+} else if (score >= 80 && score < 90) {
+    grade = 'B';
+} else {
+    grade = 'A';
+}
+
+console.log('Баллы:', score);
+console.log('Оценка:', grade);
