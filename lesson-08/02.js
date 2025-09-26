478c68e5660a12d053e1eafadf82b01d98c953e3
Gunko Vika <vikahunko@icloud.com>
2025-09-27 01:05:39 +0300

    02

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -1,14 +1,23 @@
 /*
- * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет ли эта строка корректное числовое значение.
- * Если строка является числом, функция должна возвращать true, в противном случае - false.
+ * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет
+ * ли эта строка корректное числовое значение.
+ * Если строка является числом, функция должна возвращать true, в противном
+ * случае - false.
  */
 
 function isNumeric(str) {
-  // your code
+    let result = true
+    for (let i = 0; i < str.length; i++) {
+        if (str[i]!=="." && (isNaN(str[i]) || str[i]===' ' )){
+            result = false
+            break
+        }
+    }
+    return result
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+ console.log(isNumeric("123")) // Ожидаемый результат: true
+ console.log(isNumeric("12.3")) // Ожидаемый результат: true
+ console.log(isNumeric("123abc")) // Ожидаемый результат: false
+ console.log(isNumeric("abc")) // Ожидаемый результат: false
+ console.log(isNumeric(" ")) // Ожидаемый результат: false
